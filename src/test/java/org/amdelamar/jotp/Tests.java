9f3c358c1a70d73cf0f1c7e118ae42327d63a5a2
amdelamar <amdelamar@protonmail.com>
2017-05-17 19:04:36 -0400

    Cleanup JUnit tests

--- src/test/java/org/amdelamar/jotp/Tests.java
+++ src/test/java/org/amdelamar/jotp/Tests.java
@@ -3,6 +3,9 @@ package org.amdelamar.jotp;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 
+import java.io.IOException;
+
+import org.amdelamar.jotp.exception.BadOperationException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -11,42 +14,53 @@ import org.junit.runners.JUnit4;
 public class Tests {
 
     @Test
-    public void testTOTP() {
-        try {
-            String secret = "HelloWorld";
-            String code1 = OTP.createTotp(secret, "" + System.currentTimeMillis(), 6);
+    public void encodeTests() {
 
-            // 30 sec window, so wait just 1 second
-            // This will output a different base value
-            Thread.sleep(1000);
+        // run 5 tests
+        for (int i = 0; i < 5; i++) {
+            System.out.println(OTP.random("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", 12));
+        }
 
-            String code2 = OTP.createTotp(secret, "" + System.currentTimeMillis(), 6);
-
-            // compare OTP codes
-            assertEquals(code1, code2);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+        // run 5 tests
+        for (int i = 0; i < 5; i++) {
+            System.out.println(OTP.randomBase32(OTP.BYTES));
         }
     }
 
     @Test
-    public void testHOTP() {
-        try {
-            String secret = "HelloWorld";
-            String code1 = OTP.createHotp(secret, "1", 6);
+    public void totpTests() throws BadOperationException, IOException, InterruptedException {
+
+        // run 5 tests
+        for (int i = 0; i < 5; i++) {
+            String secret = OTP.randomBase32(OTP.BYTES);
+            String code1 = OTP.create(secret, OTP.timeInHex(), 6, "totp");
+
+            // 30 sec window, so wait just a second
+            // If its beyond 30sec since the first OTP,
+            // then we will get a different base value.
+            Thread.sleep(500);
+
+            String code2 = OTP.create(secret, OTP.timeInHex(), 6, "totp");
+            assertEquals(code1, code2);
+        }
+    }
+
+    @Test
+    public void hotpTests() throws BadOperationException {
+
+        // run 5 tests
+        for (int i = 0; i < 5; i++) {
+            String secret = OTP.randomBase32(OTP.BYTES);
+            String code1 = OTP.create(secret, "1", 6, "hotp");
 
             // Indefinite window of opportunity here.
             // Next generated code SHOULD be different than the previous.
-            Thread.sleep(1000);
 
-            String code2 = OTP.createHotp(secret, "2", 6);
+            String code2 = OTP.create(secret, "1", 6, "hotp");
+            assertEquals(code1, code2);
 
-            // compare OTP codes
-            assertNotEquals(code1, code2);
-
-        } catch (Exception e) {
-            e.printStackTrace();
+            String code3 = OTP.create(secret, "2", 6, "hotp");
+            assertNotEquals(code1, code3);
         }
     }
 
