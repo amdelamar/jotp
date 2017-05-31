24c887d5b320d09c9e15b93a2159f1d716738537
amdelamar <amdelamar@protonmail.com>
2017-05-31 19:44:29 -0400

    Add random and time tests

--- src/test/java/com/amdelamar/jotp/Tests.java
+++ src/test/java/com/amdelamar/jotp/Tests.java
@@ -2,6 +2,7 @@ package com.amdelamar.jotp;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
+import static org.junit.Assert.assertNotNull;
 
 import java.io.IOException;
 
@@ -11,9 +12,47 @@ import org.junit.runners.JUnit4;
 
 import com.amdelamar.jotp.OTP;
 import com.amdelamar.jotp.exception.BadOperationException;
+import com.amdelamar.jotp.type.Type;
 
+/**
+ * Unit tests for Jotp
+ * 
+ * @author amdelamar
+ * @since 1.0.0
+ */
 @RunWith(JUnit4.class)
 public class Tests {
+
+    @Test
+    public void randomTests() {
+
+        assertNotNull(OTP.randomBase32(20));
+
+        String r1 = OTP.randomBase32(20);
+        String r2 = OTP.randomBase32(20);
+        assertNotEquals(r1, r2);
+
+        assertNotNull(OTP.random("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", 12));
+
+        String r3 = OTP.random("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", 12);
+        String r4 = OTP.random("ABCDEFGHIJKLMNOPQRSTUVWXYZ234567", 12);
+        assertNotEquals(r3, r4);
+    }
+
+    @Test
+    public void timeTests() throws BadOperationException, IOException, InterruptedException {
+
+        String t1 = OTP.timeInHex();
+        String t2 = OTP.timeInHex();
+
+        // wait a half second
+        Thread.sleep(500);
+
+        String t3 = OTP.timeInHex();
+
+        assertEquals(t1, t2);
+        assertEquals(t1, t3);
+    }
 
     @Test
     public void encodeTests() {
@@ -35,14 +74,16 @@ public class Tests {
         // run 5 tests
         for (int i = 0; i < 5; i++) {
             String secret = OTP.randomBase32(OTP.BYTES);
-            String code1 = OTP.create(secret, OTP.timeInHex(), 6, "totp");
+            String code1 = OTP.create(secret, OTP.timeInHex(), 6, Type.TOTP);
+
+            System.out.println(OTP.timeInHex());
 
             // 30 sec window, so wait just a second
             // If its beyond 30sec since the first OTP,
             // then we will get a different base value.
             Thread.sleep(500);
 
-            String code2 = OTP.create(secret, OTP.timeInHex(), 6, "totp");
+            String code2 = OTP.create(secret, OTP.timeInHex(), 6, Type.TOTP);
             assertEquals(code1, code2);
         }
     }
@@ -53,15 +94,15 @@ public class Tests {
         // run 5 tests
         for (int i = 0; i < 5; i++) {
             String secret = OTP.randomBase32(OTP.BYTES);
-            String code1 = OTP.create(secret, "1", 6, "hotp");
+            String code1 = OTP.create(secret, "1", 6, Type.HOTP);
 
             // Indefinite window of opportunity here.
             // Next generated code SHOULD be different than the previous.
 
-            String code2 = OTP.create(secret, "1", 6, "hotp");
+            String code2 = OTP.create(secret, "1", 6, Type.HOTP);
             assertEquals(code1, code2);
 
-            String code3 = OTP.create(secret, "2", 6, "hotp");
+            String code3 = OTP.create(secret, "2", 6, Type.HOTP);
             assertNotEquals(code1, code3);
         }
     }
