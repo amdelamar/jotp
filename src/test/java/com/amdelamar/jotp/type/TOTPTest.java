9e015e40b5fc675404375eea38f3518fbf685c9f
amdelamar <amdelamar@protonmail.com>
2018-06-24 12:02:19 -0400

    Add padLeft tests

--- src/test/java/com/amdelamar/jotp/type/TOTPTest.java
+++ src/test/java/com/amdelamar/jotp/type/TOTPTest.java
@@ -51,4 +51,15 @@ public class TOTPTest {
             assertTrue(OTP.verify(secret, OTP.timeInHex(), code2, 6, Type.TOTP));
         }
     }
+    
+    @Test
+    public void padLeft() throws InvalidKeyException, IllegalArgumentException, NoSuchAlgorithmException, IOException {
+        String secret = OTP.randomBase32(OTP.BYTES);
+        String code1 = OTP.create(secret, OTP.timeInHex(), 16, Type.TOTP);
+        
+        // code padded with 00's until it meets length desired
+        // e.g. 0000001868692305
+        assertEquals(16, code1.length());
+        assertTrue(code1.startsWith("0"));
+    }
 }
